b08231c0de1d2a91e42093797a04a093a0da9b3e
Elizabeth Flournoy <bflournoy@gmail.com>
2017-09-11 11:30:44 -0600

    update reduce

--- reduce/core.js
+++ reduce/core.js
@@ -5,24 +5,36 @@ function sum (array) {
   return sum;
 };
 
+
 function productAll (array) {
-  return array.reduce(function(outerAcc, outerValue){
-    return outerValue.reduce(function(innerAcc, innerVal){
+  return array.reduce((outerAcc, outerValue)=>{
+    return outerAcc*outerValue.reduce((innerAcc, innerVal)=>{
       return innerAcc*innerVal;
-    },1)
+    })
   },1)
 };
 
 function objectify (array) {
   return array.reduce(function(acc,currentVal){
-    acc[currentVAl[0]] = currentVal[1];
+    acc[currentVal[0]] = currentVal[1];
     return acc;
   },{})
 };
+// function objectify (array) {
+//   return array.reduce((a, b) => (a[b[0]] = b[1]) && a, {});
+// }
 
 function luckyNumbers (array) {
-  // your code here
+  console.log(array)
+  return array.reduce((acc, val)=>{
+    return `${acc} ${val}`
+  }, "Your lucky numbers are: ")
 };
+// function luckyNumbers (array) {
+//   return array.reduce((a, b, i) => i === array.length - 1 ? `${a} and ${b}` : `${a} ${b},`, 'Your lucky numbers are:');
+// }
+
+
 
 module.exports = {
   sum: sum,
